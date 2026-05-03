0250bec009a8173553d75b91084f364b9227dfd3
Mario Reder <mario.reder@pm.me>
2026-05-03 15:30:37 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -27,7 +27,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "ElahsDex",
   "VITE_SEO_SITE_DESCRIPTION": "Great perp - dex can trade anything in the world",
-  "VITE_SEO_SITE_URL": "https://elahsdex.com",
+  "VITE_SEO_SITE_URL": "https://elahsdex1.com",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
